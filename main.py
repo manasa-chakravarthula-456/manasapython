a4250ce4130427f93d03daa46dc32380741b30ad
manasa-chakravarthula-456 <manasachakravathula@gmail.com>
2025-12-26 13:51:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+x="madam"
+l=0
+r=len(x)-1
+while l<r:
+    if x[1]==x[r]:
+        l+=-1
+        r-=1
+        print("palindrome")
+    else:
+        print("not a paindrome")
